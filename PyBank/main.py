9390abb785991ae0bf25f6d3daaa118da2087344
kiErnguyen <k@arbelo.io>
2023-01-18 19:34:22 -0800

    CHALLENGE COMPLETE!

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,10 +5,12 @@
 # Importing necessary module(s).
 import csv
 
+# Defining .csv file.
 path = '/home/kiernguyen/Desktop/python-challenge/PyBank/Resources/budget_data.csv'
 file = open(path)
 reader = csv.reader(file)
 
+# Creating a list of lists from the .csv file.
 header = next(reader)
 
 data = []
@@ -17,28 +19,34 @@ for row in reader:
     profit_losses = int(row[1])
     data.append([date, profit_losses])
 
+# Separating months into their own list.
 months = []
 for row in data:
     months.append(row[0])
 
+# Separating profit/ losses into their own list, as well.
 total_profit_losses = []
 for row in data:
     total_profit_losses.append(row[1])
 total = sum(total_profit_losses)
 
+# Calculating monthly change and finding the average.
 monthly_change = []
 for i in range(len(total_profit_losses)-1):
     monthly_change.append(total_profit_losses[i+1]-total_profit_losses[i])
 average_change = round(sum(monthly_change) / (len(months) - 1), 2)
 
+# Calculating and defining the month with the greats increase in profits.
 greatest_increase = max(monthly_change)
 increase_month = monthly_change.index(max(monthly_change)) +1
 greatest_increase_in_profits = (months[increase_month])
 
+# Calculating and defining the month that suffered the greatest loss.
 greatest_decrease = min(monthly_change)
 decrease_month = monthly_change.index(min(monthly_change)) +1
 greatest_decrease_in_profits = (months[decrease_month])
 
+# Print functions outlining the results.
 print('Financial Analysis')
 print('----------')
 print('Total Months: ' + str(len(months)))
@@ -46,6 +54,7 @@ print('Average Change: $' + str(average_change))
 print('Greatest Increase in Profits: ' + greatest_increase_in_profits + ' ($' + str(greatest_increase) +')')
 print('Greatest Decrease in Profits: ' + greatest_decrease_in_profits + ' ($' + str(greatest_decrease) +')')
 
+# Writing results to a .txt file.
 analysis = '/home/kiernguyen/Desktop/python-challenge/PyBank/analysis/analysis.txt'
 
 with open(analysis, 'w') as file:
@@ -55,3 +64,7 @@ with open(analysis, 'w') as file:
     file.write('Average Change: $' + str(average_change))
     file.write('Greatest Increase in Profits: ' + greatest_increase_in_profits + ' ($' + str(greatest_increase) + ')')
     file.write('Greatest Decrease in Profits: ' + greatest_decrease_in_profits + ' ($' + str(greatest_decrease) + ')')
+
+
+
+# ễ
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,15 +5,13 @@
 # Importing necessary module(s).
 import csv
 
+# Defining .csv file.
 path = '/home/kiernguyen/Desktop/python-challenge/PyPoll/Resources/election_data.csv'
 file = open(path)
 reader = csv.reader(file)
 
+# Creating a list of lists from the .csv file.
 header = next(reader)
-
-Charles = 0
-Diana = 0
-Raymon = 0
 
 data = []
 for row in reader:
@@ -22,6 +20,12 @@ for row in reader:
     Candidate = (row[2])
     data.append([Ballot_ID, County, Candidate])
 
+# Creating variables for the following loop.
+Charles = 0
+Diana = 0
+Raymon = 0
+
+# Loop using if and elif statements to count votes for each candidate.
 for row in data:
     if row[2] == 'Charles Casper Stockham':
         Charles += 1
@@ -30,17 +34,42 @@ for row in data:
     elif row[2] == 'Raymon Anthony Doane':
         Raymon += 1
 
+# Calculating the percentage of votes per candidate.
 total_votes = Charles + Diana + Raymon
 
 Charles_percent = round(Charles / total_votes * 100, 3)
 Diana_percent = round(Diana / total_votes * 100, 3)
 Raymon_percent = round(Raymon / total_votes * 100, 3)
 
+# Leveraging a dictionary to find the winner.
 candidate_votes = {'Charles Casper Stockham': Charles, 'Diane DeGette': Diana, 'Raymon Anthony Doane': Raymon}
 winner = max(candidate_votes, key=candidate_votes.get)
 
+# Print functions outlining the results.
 print('Election Results')
 print('----------')
 print('Total Votes: ' + str(total_votes))
 print('----------')
 print('Charles Casper Stockham: ' + str(Charles_percent) + '% (' + str(Charles) + ')')
+print('Diana DeGette: ' + str(Diana_percent) + '% (' + str(Diana) + ')')
+print('Raymon Anthony Doane: ' + str(Raymon_percent) + '% (' + str(Raymon) + ')')
+print('----------')
+print('Winner: ' + winner)
+
+# Writing the results to a .txt file.
+analysis = '/home/kiernguyen/Desktop/python-challenge/PyPoll/analysis/analysis.txt'
+
+with open(analysis, 'w') as file:
+    file.write('Election Results')
+    file.write('----------')
+    file.write('Total Votes: ' + str(total_votes))
+    file.write('----------')
+    file.write('Charles Casper Stockham: ' + str(Charles_percent) + '% (' + str(Charles) + ')')
+    file.write('Diana DeGette: ' + str(Diana_percent) + '% (' + str(Diana) + ')')
+    file.write('Raymon Anthony Doane: ' + str(Raymon_percent) + '% (' + str(Raymon) + ')')
+    file.write('----------')
+    file.write('Winner: ' + winner)
+
+
+
+# ễ
